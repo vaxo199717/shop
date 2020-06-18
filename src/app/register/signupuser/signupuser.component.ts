e9db7d7ea6a102568f61d4b69c33877f1063c6ae
lashaTS <mkvaxi@gmail.com>
2020-06-18 14:30:15 +0400

    dsfsdsd

--- src/app/register/signupuser/signupuser.component.ts
+++ src/app/register/signupuser/signupuser.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-  
+
 
 @Component({
   selector: 'app-signupuser',
@@ -8,11 +8,26 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./signupuser.component.scss']
 })
 export class SignupuserComponent implements OnInit {
-bday: Number[]=[];
-bmonth: string[]=['იანვარი', 'თებერვალი', 'მარტი', 'აპრილი', 'მაისი', 'ივნისი', 'ივლისი', 'აგვისტო', 'სექტემბერი', 'ოქტომბერი', 'ნოემბერი', 'დეკემბერი'];
-byear: number[]=[];
-forms: FormGroup;
-  constructor(fb: FormBuilder) { 
+  bday: Number[] = [];
+  bmonth: string[] = ['იანვარი', 'თებერვალი', 'მარტი', 'აპრილი', 'მაისი', 'ივნისი', 'ივლისი', 'აგვისტო', 'სექტემბერი', 'ოქტომბერი', 'ნოემბერი', 'დეკემბერი'];
+  byear: number[] = [];
+  forms: FormGroup;
+
+  validatormessage = {
+    'firstName': {
+      'requaired': 'შეიყვანეთ სახელი!!!',
+    },
+    'lastName': {
+      'requaired': 'შეიყვანეთ გვარი!!!',
+    },
+    'numOrEmail': {
+      'requaired': 'შეიყვანეთ ნომერ ან ელ-ფოსტა!!!',
+    },
+    'password': {
+      'requaired': 'შეიყვანეთ პაროლი!!!',
+    }
+  }
+  constructor(fb: FormBuilder) {
     this.forms = fb.group({
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
@@ -26,15 +41,15 @@ forms: FormGroup;
   }
 
   ngOnInit(): void {
-    for(let i=1; i<=31; i++){
+    for (let i = 1; i <= 31; i++) {
       this.bday.push(i);
     }
-    for(let i=1920; i<=2020; i++){
+    for (let i = 1920; i <= 2020; i++) {
       this.byear.push(i);
     }
 
   }
-  add():void{
+  add(): void {
     console.log(this.forms.value)
   }
 }
